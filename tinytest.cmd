0181b5c939adc70bea1a199e38429974a86339b5
HAYAMA_Kaoru <iyahaya@nifty.com>
2024-03-19 01:06:50 +0900

    Modify tinytest for new commandline parameter

--- tinytest.cmd
+++ tinytest.cmd
@@ -10,12 +10,11 @@
 @echo ***
 @echo *** DOWNLOAD TEST
 @echo ***
-make-scoop-manifest.exe -D -g hymkor/make-scoop-manifest > tmp.json
-fc tmp.json make-scoop-manifest.json || exit /b 1
+make-scoop-manifest.exe hymkor/make-scoop-manifest > tmp.json
+fc tmp.json make-scoop-manifest.json
 @
 @echo ***
 @echo *** LOCAL ZIP TEST
 @echo ***
-make-scoop-manifest.exe .\dist\make-scoop-manifest-v0.9.0-windows-*.zip > tmp.json
+make-scoop-manifest.exe .\dist\make-scoop-manifest-*-windows-*.zip > tmp.json
 fc tmp.json make-scoop-manifest.json
-@exit /b %ERRORLEVEL%
